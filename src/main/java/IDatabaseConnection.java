dbfd6ce4ac37eb82e7b1bd0d0786bc1cc8bd9537
David Fairbrother <DavidFair@users.noreply.github.com>
2018-10-24 21:27:00 +0100

    Add example files to build

--- src/main/java/IDatabaseConnection.java
+++ src/main/java/IDatabaseConnection.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.io.IOException;
+import java.net.ConnectException;
 
 public interface IDatabaseConnection {
+    void connectToDatabase() throws ConnectException;
+
+    void disconnectFromDb() throws ConnectException;
+
+    void addLogData(LogData newLogEntry) throws IOException;
+
+    LogData getLogEntry(String logId) throws IOException;
+
+    LogData findLogEntry(LogData searchCriteria) throws IOException;
+
+    void removeLogData(String logId) throws IOException;
 }
--- src/main/java/LogData.java
+++ src/main/java/LogData.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import java.util.Date;
 
 public class LogData {
+    public String logId;
+    public Date timestamp;
+    public LoggingLevel logLevel;
+    public String content;
+    public String userId;
 }
