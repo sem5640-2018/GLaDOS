356a8d99abff2fad30cf3884d5fd3f563daf14b4
David Fairbrother <DavidFair@users.noreply.github.com>
2018-10-27 03:12:53 +0100

    Add custom handler for JsonParseExceptions

---
+++ src/main/java/uk/ac/aber/dcs/aberfitness/glados/api/JsonParseExceptionWrapper.java
@@ -0,0 +1,20 @@
+package uk.ac.aber.dcs.aberfitness.glados.api;
+
+import com.google.gson.JsonParseException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+
+/**
+ * Converts IOExceptions into InternalServer Error responses within the
+ * Javax Ws layer
+ */
+@Provider
+public class JsonParseExceptionWrapper implements ExceptionMapper<JsonParseException> {
+    @Override
+    public Response toResponse(JsonParseException exception) {
+        return Response.status(Response.Status.BAD_REQUEST).build();
+    }
+}
