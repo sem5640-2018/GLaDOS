890c49ad1adcd4fcfe51853cd8e97e998d35bb0e
David Fairbrother <DavidFair@users.noreply.github.com>
2018-11-12 18:53:23 +0000

    Update GET methods to return responses

--- src/main/java/uk/ac/aber/dcs/aberfitness/glados/api/AuditApi.java
+++ src/main/java/uk/ac/aber/dcs/aberfitness/glados/api/AuditApi.java
@@ -9,16 +9,16 @@ import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.json.JsonObject;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.List;
 
 @Stateless
-@Path("log")
+@Path("audit")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AuditApi {
@@ -26,16 +26,32 @@ public class AuditApi {
     DatabaseConnection dbConnection;
 
     @GET
-    public JsonArray getAllEntries() throws IOException {
+    @Path("/all")
+    public Response getAllEntries(@QueryParam("from") int from,
+                                  @QueryParam("to") int to,
+                                  @QueryParam("orderBy") String orderBy) throws IOException {
         // TODO add limits / ranges
         final JsonArrayBuilder jsonArray = Json.createArrayBuilder();
         List<AuditData> foundEntries = dbConnection.getAllLogEntries();
 
         // Ensure we have JSON serialisable elements
         foundEntries.forEach(e->jsonArray.add(new AuditDataJson(e).toJson()));
-        return jsonArray.build();
+        JsonArray json = jsonArray.build();
+
+        return Response.ok(json.toString(), MediaType.APPLICATION_JSON).build();
     }
 
+    @GET
+    public Response getAuditById(@QueryParam("logId") String logId) throws IOException {
+        AuditData foundEntry = dbConnection.getLogEntry(logId);
+
+        if (foundEntry == null){
+            return Response.status(404, "Not Found").build();
+        }
+
+        JsonObject json = new AuditDataJson(foundEntry).toJson();
+        return Response.ok(json.toString(), MediaType.APPLICATION_JSON).build();
+    }
 
 
 }
--- src/test/java/UnitTest/AuditApiTest.java
+++ src/test/java/UnitTest/AuditApiTest.java
@@ -1,9 +1,14 @@
 package UnitTest;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
 import helpers.AuditDataHelpers;
+import org.apache.logging.log4j.core.Core;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
@@ -11,11 +16,16 @@ import uk.ac.aber.dcs.aberfitness.glados.api.AuditApi;
 import uk.ac.aber.dcs.aberfitness.glados.db.*;
 
 import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
+import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 public class AuditApiTest {
@@ -36,28 +46,59 @@ public class AuditApiTest {
      * Returns a sample AuditData entry for unit testing
      * @return A sample AuditData object
      */
-    private AuditData createExampleLogData() {
+    private AuditData createExampleLogData(String userId) {
         return new AuditDataNoSerial(Instant.now(), LoggingLevels.DEBUG, "TestContent",
-                "abc123", ServiceNames.GLADOS);
+                userId, ServiceNames.GLADOS);
     }
 
     @Test
     public void findAllCallReturnsAll() throws IOException {
-        AuditData dummyLogOne = createExampleLogData();
-        AuditData dummyLogTwo = createExampleLogData();
+        String userOne = "abc123";
+        String userTwo = "bcd234";
+        AuditData dummyLogOne = createExampleLogData(userOne);
+        AuditData dummyLogTwo = createExampleLogData(userTwo);
 
         List<AuditData> mockedData = Arrays.asList(dummyLogOne, dummyLogTwo);
         when(dbMock.getAllLogEntries()).thenReturn(mockedData);
 
         // This should return JSON
-        JsonArray returnedJson = apiInstance.getAllEntries();
-        List<AuditDataJson> returnedEntries = AuditDataJson.fromJson(returnedJson);
+        Response returnedBody = apiInstance.getAllEntries(0, 10, "any");
+        String returnedJson = (String)returnedBody.getEntity();
 
-        Assert.assertEquals(returnedEntries.size(), mockedData.size());
+        Assert.assertThat(returnedJson, CoreMatchers.containsString(userOne));
+        Assert.assertThat(returnedJson, CoreMatchers.containsString(userTwo));
+    }
 
-        for (int i = 0; i < returnedEntries.size(); i++) {
-            AuditDataHelpers.isAlmostEqual(returnedEntries.get(i), mockedData.get(i));
-        }
+    @Test
+    public void getLogById() throws IOException {
+        String userId = "test1";
+        AuditData newLogData = createExampleLogData(userId);
+        String testId = newLogData.getLogId();
+
+        ArgumentCaptor<String> arg = ArgumentCaptor.forClass(String.class);
+        when(dbMock.getLogEntry(arg.capture())).thenReturn(newLogData);
+
+        String returnedJson = (String)apiInstance.getAuditById(testId).getEntity();
+
+        // Check arg forwards correctly
+        Assert.assertEquals(testId, arg.getValue());
+
+        // Check result
+        Assert.assertThat(returnedJson, CoreMatchers.containsString(userId));
+    }
+
+    @Test
+    public void getLogByIdDoesNotExist() throws IOException {
+        String doesNotExist = "bar";
+
+        ArgumentCaptor<String> arg = ArgumentCaptor.forClass(String.class);
+        when(dbMock.getLogEntry(arg.capture())).thenReturn(null);
+
+        Response returned = apiInstance.getAuditById(doesNotExist);
+        String returnedJson = (String)returned.getEntity();
+
+        // Check result
+        Assert.assertEquals(404, returned.getStatus());
     }
 
 
