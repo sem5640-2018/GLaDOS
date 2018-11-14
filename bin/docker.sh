d353e47110ff3f2ef92c59f717931dad3f987735
David Fairbrother <DavidFair@users.noreply.github.com>
2018-11-14 14:50:00 +0000

    Add docker username and password to Travis

--- bin/docker.sh
+++ bin/docker.sh
@@ -1,4 +1,4 @@
-echo "$ENCRYPTED_DOCKER_PASSWORD" | docker login -u "$ENCRYPTED_DOCKER_USERNAME" --password-stdin
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 cd Gatekeeper
 docker build -t sem56402018/gatekeeper:$1 -t sem56402018/gatekeeper:$TRAVIS_COMMIT .
 docker push sem56402018/gatekeeper:$TRAVIS_COMMIT
