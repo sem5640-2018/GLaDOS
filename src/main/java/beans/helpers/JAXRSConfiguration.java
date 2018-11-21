641418d3e74712acc98c86b0e3c1c0c6bb0f7d69
David Fairbrother <DavidFair@users.noreply.github.com>
2018-11-21 21:32:08 +0000

    Move base endpoint back to /api from /entities

--- src/main/java/beans/helpers/JAXRSConfiguration.java
+++ src/main/java/beans/helpers/JAXRSConfiguration.java
@@ -3,6 +3,6 @@ package beans.helpers;
 import javax.ws.rs.ApplicationPath;
 
 // Defines the base endpoint path
-@ApplicationPath("entities")
+@ApplicationPath("api")
 public class JAXRSConfiguration {
 }
