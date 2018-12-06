bff82f168436822fe9ee37ace660177e81adbeff
David Fairbrother <DavidFair@users.noreply.github.com>
2018-12-06 03:20:18 +0000

    Unused imports again

--- src/main/java/beans/IndexBacking.java
+++ src/main/java/beans/IndexBacking.java
@@ -2,9 +2,7 @@ package beans;
 
 import beans.helpers.LoginCheck;
 
-import javax.annotation.PostConstruct;
 import javax.ejb.Stateless;
-import javax.enterprise.context.RequestScoped;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletResponse;
