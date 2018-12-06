3b98f8bf90f79f207b754c7e7b7c9df69aa22022
David Fairbrother <DavidFair@users.noreply.github.com>
2018-12-06 03:04:46 +0000

    Remove unused imports

--- src/main/java/beans/LoginBacking.java
+++ src/main/java/beans/LoginBacking.java
@@ -7,7 +7,6 @@ import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
--- src/main/java/beans/helpers/LoginCheck.java
+++ src/main/java/beans/helpers/LoginCheck.java
@@ -1,6 +1,5 @@
 package beans.helpers;
 
-import configuration.EnvironmentVariables;
 import oauth.GatekeeperLogin;
 import oauth.gatekeeper.GatekeeperInfo;
 
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.Map;
 
 public class LoginCheck {
 
