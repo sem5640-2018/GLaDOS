e2208cda84b4646d39c1a3c6529f797a935145a7
David Fairbrother <DavidFair@users.noreply.github.com>
2018-12-08 21:06:09 +0000

    Start implementing status beans

---
+++ src/main/java/beans/StatusBacking.java
@@ -0,0 +1,32 @@
+package beans;
+
+import beans.helpers.StatusSingleton;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Named
+@Stateless
+public class StatusBacking {
+
+
+    private List<Map.Entry<String, Boolean>> serviceStatus;
+
+    @EJB
+    StatusSingleton statusGetter;
+
+    @PostConstruct
+    void init(){
+        serviceStatus = new ArrayList<>(statusGetter.getServiceStatuses().entrySet());
+        serviceStatus.sort(Map.Entry.comparingByKey());
+    }
+
+    public List<Map.Entry<String, Boolean>> getServiceStatus() {
+        return serviceStatus;
+    }
+}
---
+++ src/main/java/beans/helpers/StatusSingleton.java
@@ -0,0 +1,99 @@
+package beans.helpers;
+
+import configuration.EnvironmentVariables;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import rest.helpers.ServiceNames;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@Startup
+@Singleton
+public class StatusSingleton {
+
+    private static final Logger log = LogManager.getLogger(StatusSingleton.class);
+
+    private CompletableFuture asyncQuery;
+    private Map<String, Boolean> serviceStatuses = new HashMap<>();
+
+    public StatusSingleton() {
+        for (ServiceNames name : ServiceNames.values()) {
+            serviceStatuses.put(name.toString().replace('_', '-'), false);
+        }
+    }
+
+    @PostConstruct
+    public void init(){
+        asyncQuery = CompletableFuture.runAsync(this::asyncQueryServices);
+    }
+
+    public Map<String, Boolean> getServiceStatuses() {
+        return serviceStatuses;
+    }
+
+    // Query services every minute in the background
+    @Schedule(hour = "*", minute = "*", second = "*/20", persistent = false)
+    private void checkQueriesCompleted() {
+        if (asyncQuery == null){
+            return;
+        }
+
+        try {
+            asyncQuery.get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException | TimeoutException | ExecutionException e) {
+            log.warn("Cancelling missed future");
+        }
+
+        if (!asyncQuery.isDone()) {
+            log.warn("Cancelling missed future");
+            asyncQuery.cancel(true);
+        }
+
+        asyncQuery = CompletableFuture.runAsync(this::asyncQueryServices);
+    }
+
+    private void asyncQueryServices() {
+        for (String names : serviceStatuses.keySet()) {
+            queryIndividualServiceWrapper(names);
+        }
+    }
+
+    private void queryIndividualServiceWrapper(String serviceToQuery) {
+        try {
+            queryIndividualService(serviceToQuery);
+        } catch (Exception e) {
+            log.warn("Exception querying service: " + e.getMessage());
+            serviceStatuses.put(serviceToQuery, false);
+        }
+    }
+
+    private void queryIndividualService(String serviceToQuery) throws IOException {
+        URL url = new URL(EnvironmentVariables.getSystemBaseUrl() + '/' + serviceToQuery + "/api/status");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+
+        connection.setConnectTimeout(500);
+        connection.setReadTimeout(500);
+
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == 200 || responseCode == 204) {
+            // Service is up
+            serviceStatuses.put(serviceToQuery, true);
+            return;
+        }
+
+        serviceStatuses.put(serviceToQuery, false);
+    }
+}
