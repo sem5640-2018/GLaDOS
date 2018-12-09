7af02c44d44d8c1ee53420e7c0cc4aff1e9e9695
David Fairbrother <DavidFair@users.noreply.github.com>
2018-12-09 19:33:39 +0000

    Switch back to generated outcomes

--- src/main/java/beans/IndexBacking.java
+++ src/main/java/beans/IndexBacking.java
@@ -1,38 +1,14 @@
 package beans;
 
-import beans.helpers.LoginCheck;
-import configuration.EnvironmentVariables;
-
 import javax.ejb.Stateless;
-import javax.faces.context.FacesContext;
 import javax.inject.Named;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Named
 @Stateless
-public class IndexBacking extends LoginCheck {
-
-    private String loginPageUrl = EnvironmentVariables.getAppBaseUrl() + "/login.xhtml";
+public class IndexBacking {
 
     public IndexBacking(){
-        super();
-    }
 
-    public void onLoad(){
-        boolean isLoggedIn = checkUserLogin();
-        if (isLoggedIn){
-            HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
-            try {
-                response.sendRedirect(EnvironmentVariables.getAppBaseUrl() + "/userDataLookup.xhtml");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public String getLoginPageUrl() {
-        return loginPageUrl;
     }
 }
 
